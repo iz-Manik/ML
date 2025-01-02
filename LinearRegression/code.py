2c91c54e6bd8405f7b3a607d9e9c0fed6e6e5eca
Manik <manikchauhan029@gmail.com>
2025-01-03 00:52:25 +0530

    initial learning

--- LinearRegression/code.py
+++ LinearRegression/code.py
@@ -3,3 +3,19 @@ from urllib.request import urlretrieve
 urlretrieve(medical_charges_url, 'medical.csv')
 import pandas as pd
 medical_df = pd.read_csv('medical.csv')
+print(medical_df)
+import plotly.express as px
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('darkgrid')
+matplotlib.rcParams['font.size'] = 14
+matplotlib.rcParams['figure.figsize'] = (10, 6)
+matplotlib.rcParams['figure.facecolor'] = '#00000000'
+fig = px.histogram(medical_df,
+                   x='age',
+                   marginal='box',
+                   nbins=47,
+                   title='Distribution of Age')
+fig.update_layout(bargap=0.1)
+fig.show()
