debe69d909674380318da550329e97a84d5d8310
Manik <manikchauhan029@gmail.com>
2025-01-07 20:14:20 +0530

    Decision tree and random forest

--- LogisticRegression/code.py
+++ LogisticRegression/code.py
@@ -185,3 +185,133 @@ def predict_and_plot(inputs, targets, name=''):
 
 train_preds = predict_and_plot(X_train, train_targets, 'Training')
 
+## Saving and Loading Trained Models
+
+# We can save the parameters (weights and biases) of our trained model to disk, so that we needn't retrain the model from scratch each time we wish to use it. Along with the model, it's also important to save imputers, scalers, encoders and even column names. Anything that will be required while generating predictions using the model should be saved.
+
+# We can use the `joblib` module to save and load Python objects on the disk.
+
+import joblib
+aussie_rain = {
+    'model': model,
+    'imputer': imputer,
+    'scaler': scaler,
+    'encoder': encoder,
+    'input_cols': input_cols,
+    'target_col': target_col,
+    'numeric_cols': numeric_cols,
+    'categorical_cols': categorical_cols,
+    'encoded_cols': encoded_cols
+}
+joblib.dump(aussie_rain, 'aussie_rain.joblib')
+aussie_rain2 = joblib.load('aussie_rain.joblib')
+
+test_preds2 = aussie_rain2['model'].predict(X_test)
+accuracy_score(test_targets, test_preds2)
+
+
+# Putting it all Together
+import opendatasets as od
+import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+
+# Download the dataset
+od.download('https://www.kaggle.com/jsphyg/weather-dataset-rattle-package')
+raw_df = pd.read_csv('weather-dataset-rattle-package/weatherAUS.csv')
+raw_df.dropna(subset=['RainToday', 'RainTomorrow'], inplace=True)
+
+# Create training, validation and test sets
+year = pd.to_datetime(raw_df.Date).dt.year
+train_df, val_df, test_df = raw_df[year < 2015], raw_df[year == 2015], raw_df[year > 2015]
+
+# Create inputs and targets
+input_cols = list(train_df.columns)[1:-1]
+target_col = 'RainTomorrow'
+train_inputs, train_targets = train_df[input_cols].copy(), train_df[target_col].copy()
+val_inputs, val_targets = val_df[input_cols].copy(), val_df[target_col].copy()
+test_inputs, test_targets = test_df[input_cols].copy(), test_df[target_col].copy()
+
+# Identify numeric and categorical columns
+numeric_cols = train_inputs.select_dtypes(include=np.number).columns.tolist()[:-1]
+categorical_cols = train_inputs.select_dtypes('object').columns.tolist()
+
+# Impute missing numerical values
+imputer = SimpleImputer(strategy = 'mean').fit(raw_df[numeric_cols])
+train_inputs[numeric_cols] = imputer.transform(train_inputs[numeric_cols])
+val_inputs[numeric_cols] = imputer.transform(val_inputs[numeric_cols])
+test_inputs[numeric_cols] = imputer.transform(test_inputs[numeric_cols])
+
+# Scale numeric features
+scaler = MinMaxScaler().fit(raw_df[numeric_cols])
+train_inputs[numeric_cols] = scaler.transform(train_inputs[numeric_cols])
+val_inputs[numeric_cols] = scaler.transform(val_inputs[numeric_cols])
+test_inputs[numeric_cols] = scaler.transform(test_inputs[numeric_cols])
+
+# One-hot encode categorical features
+encoder = OneHotEncoder(sparse=False, handle_unknown='ignore').fit(raw_df[categorical_cols])
+encoded_cols = list(encoder.get_feature_names_out(categorical_cols))
+train_inputs[encoded_cols] = encoder.transform(train_inputs[categorical_cols])
+val_inputs[encoded_cols] = encoder.transform(val_inputs[categorical_cols])
+test_inputs[encoded_cols] = encoder.transform(test_inputs[categorical_cols])
+
+# Save processed data to disk
+train_inputs.to_parquet('train_inputs.parquet')
+val_inputs.to_parquet('val_inputs.parquet')
+test_inputs.to_parquet('test_inputs.parquet')
+pd.DataFrame(train_targets).to_parquet('train_targets.parquet')
+pd.DataFrame(val_targets).to_parquet('val_targets.parquet')
+pd.DataFrame(test_targets).to_parquet('test_targets.parquet')
+
+# Load processed data from disk
+train_inputs = pd.read_parquet('train_inputs.parquet')
+val_inputs = pd.read_parquet('val_inputs.parquet')
+test_inputs = pd.read_parquet('test_inputs.parquet')
+train_targets = pd.read_parquet('train_targets.parquet')[target_col]
+val_targets = pd.read_parquet('val_targets.parquet')[target_col]
+test_targets = pd.read_parquet('test_targets.parquet')[target_col]
+
+
+#Model training and evaluation
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import joblib
+
+# Select the columns to be used for training/prediction
+X_train = train_inputs[numeric_cols + encoded_cols]
+X_val = val_inputs[numeric_cols + encoded_cols]
+X_test = test_inputs[numeric_cols + encoded_cols]
+
+# Create and train the model
+model = LogisticRegression(solver='liblinear')
+model.fit(X_train, train_targets)
+
+# Generate predictions and probabilities
+train_preds = model.predict(X_train)
+train_probs = model.predict_proba(X_train)
+accuracy_score(train_targets, train_preds)
+
+# Helper function to predict, compute accuracy & plot confustion matrix
+def predict_and_plot(inputs, targets, name=''):
+    preds = model.predict(inputs)
+    accuracy = accuracy_score(targets, preds)
+    print("Accuracy: {:.2f}%".format(accuracy * 100))
+    cf = confusion_matrix(targets, preds, normalize='true')
+    plt.figure()
+    sns.heatmap(cf, annot=True)
+    plt.xlabel('Prediction')
+    plt.ylabel('Target')
+    plt.title('{} Confusion Matrix'.format(name));
+    return preds
+
+# Evaluate on validation and test set
+val_preds = predict_and_plot(X_val, val_targets, 'Validation')
+test_preds = predict_and_plot(X_test, test_targets, 'Test')
+
+# Save the trained model & load it back
+aussie_rain = {'model': model, 'imputer': imputer, 'scaler': scaler, 'encoder': encoder,
+               'input_cols': input_cols, 'target_col': target_col, 'numeric_cols': numeric_cols,
+               'categorical_cols': categorical_cols, 'encoded_cols': encoded_cols}
+joblib.dump(aussie_rain, 'aussie_rain.joblib')
+aussie_rain2 = joblib.load('aussie_rain.joblib')
