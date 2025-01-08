1a2a3b33d1d67c003446f1c9cb7b5b40141ea117
Manik <manikchauhan029@gmail.com>
2025-01-08 17:19:05 +0530

    model train

--- Decision_Tree/code.py
+++ Decision_Tree/code.py
@@ -34,4 +34,67 @@ raw_df.dropna(subset=['RainTomorrow'], inplace=True)
 # 5. Scale numeric values to the $(0, 1)$ range
 # 6. Encode categorical columns to one-hot vectors
 
+plt.title('No. of Rows per Year')
+sns.countplot(x=pd.to_datetime(raw_df.Date).dt.year)
+plt.show()
 
+year = pd.to_datetime(raw_df.Date).dt.year
+
+train_df = raw_df[year < 2015]
+val_df = raw_df[year == 2015]
+test_df = raw_df[year > 2015]
+
+input_cols = list(train_df.columns)[1:-1]
+target_col = 'RainTomorrow'
+
+train_inputs = train_df[input_cols].copy()
+train_targets = train_df[target_col].copy()
+
+val_inputs = val_df[input_cols].copy()
+val_targets = val_df[target_col].copy()
+
+test_inputs = test_df[input_cols].copy()
+test_targets = test_df[target_col].copy()
+
+numeric_cols = train_inputs.select_dtypes(include=np.number).columns.tolist()
+categorical_cols = train_inputs.select_dtypes('object').columns.tolist()
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(strategy = 'mean').fit(raw_df[numeric_cols])
+train_inputs[numeric_cols] = imputer.transform(train_inputs[numeric_cols])
+val_inputs[numeric_cols] = imputer.transform(val_inputs[numeric_cols])
+test_inputs[numeric_cols] = imputer.transform(test_inputs[numeric_cols])
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler().fit(raw_df[numeric_cols])
+train_inputs[numeric_cols] = scaler.transform(train_inputs[numeric_cols])
+val_inputs[numeric_cols] = scaler.transform(val_inputs[numeric_cols])
+test_inputs[numeric_cols] = scaler.transform(test_inputs[numeric_cols])
+
+from sklearn.preprocessing import OneHotEncoder
+encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore').fit(raw_df[categorical_cols])
+encoded_cols = list(encoder.get_feature_names_out(categorical_cols))
+train_inputs[encoded_cols] = encoder.transform(train_inputs[categorical_cols])
+val_inputs[encoded_cols] = encoder.transform(val_inputs[categorical_cols])
+test_inputs[encoded_cols] = encoder.transform(test_inputs[categorical_cols])
+
+X_train = train_inputs[numeric_cols + encoded_cols]
+X_val = val_inputs[numeric_cols + encoded_cols]
+X_test = test_inputs[numeric_cols + encoded_cols]
+print(X_test)
+
+from sklearn.tree import DecisionTreeClassifier
+model = DecisionTreeClassifier(random_state=42)
+model.fit(X_train, train_targets)
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+train_preds = model.predict(X_train)
+
+train_probs = model.predict_proba(X_train)
+accuracy_score(train_targets, train_preds)
+
+model.score(X_val, val_targets)
+
+from sklearn.tree import plot_tree, export_text
+plt.figure(figsize=(80,20))
+plot_tree(model, feature_names=X_train.columns, max_depth=2, filled=True);
